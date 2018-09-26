45ef35fa281db2ab6c6210054884a606f4de103e
misausaki <xoxo.1826@gmail.com>
2018-09-26 20:19:27 +0900

    test version01

---
+++ script.js
@@ -0,0 +1,102 @@
+/*
+
+document.write("1)<br>");
+for(var a=1; a<=5; a++){
+    document.write("★");
+}
+document.write("<br>");
+
+document.write("2)<br>");
+
+for(var b=1; b<=2; b++){
+    for(var c=1; c<=3; c++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+
+document.write("3)<br>");
+
+for(var e=1; e<=2; e++){
+    for(var f=1; f<=5; f++){
+        document.write("☆");
+    }
+    document.write("<br>");
+}
+
+document.write("4)<br>");
+
+
+
+
+
+
+for(var g=1; g<=4; g++){
+    for(var h=1; h<=5; h++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+
+
+document.write("5)<br>");
+
+
+
+for(var i=1; i<=4; i++){
+    for(var j=1; j<=3; j++){
+        document.write("★");
+    }
+    document.write("<br>");
+}
+   
+   
+  
+  
+ document.write("6)<br>");
+ 
+ */
+ for(var k=1; k<3=; k++){
+    var(var l=0; l<=2; l++){
+    if(l%2==0){
+    document.write("★");
+    }else{
+    document.write("☆");
+    }
+    }
+    document.write("<br>");
+ }
+  
+  
+ /* 
+  document.write("7)<br>");
+  
+  for(var m=1; m<=4; m++){
+    var(var n=0; n<=4; n++){
+    if(l%2==0){
+    document.write("★");
+    }else{
+    document.write("☆");
+    }
+    }
+    document.write("<br>");
+ }
+    
+    
+    
+    
+ document.write("8)<br>");
+ 
+ var star="★";
+ for(var o=1; o<=5; o++){
+    for(va)
+ }
+    
+    
+    
+    
+    
+    
+}*/
